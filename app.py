e2d0a88074fa4605fa31c6805ab5875a2706de08
Pinchai <>
2024-03-06 19:10:59 +0700

    config template

--- app.py
+++ app.py
@@ -4,8 +4,21 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def index():
+    module = 'master'
+    return render_template('master.html', module=module)
+
+
+@app.route('/dashboard')
+def dashboard():
+    module = 'dashboard'
+    return render_template('dashboard.html', module=module)
+
+
+@app.route('/user')
+def user():
+    module = 'user'
+    return render_template('user.html', module=module)
 
 
 if __name__ == '__main__':
