fc74aa049b1a9703a3993de0842b40e729689e00
Pinchai <>
2024-03-13 19:52:06 +0700

    route part1

--- app.py
+++ app.py
@@ -2,6 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+std_list = [
+    {
+        'id': 10,
+        'name': 'pinchai',
+        'gender': 'male',
+        'phone': '099 774 967',
+        'email': 'pinchai.pc@gmail.com',
+        'address': 'PhnomPenh',
+    },
+    {
+        'id': 12,
+        'name': 'sreypich',
+        'gender': 'female',
+        'phone': '010 774 967',
+        'email': 'sreypich@gmail.com',
+        'address': 'Takeo',
+    }
+]
+
 
 @app.route('/')
 def index():
@@ -18,7 +37,15 @@ def dashboard():
 @app.route('/user')
 def user():
     module = 'user'
-    return render_template('user.html', module=module)
+    return render_template('user.html', module=module, data=std_list)
+
+
+@app.route('/edit_user/<int:user_id>')
+def edit_user(user_id):
+    module = 'user'
+    user_id = user_id
+    # return f"User ID : {user_id}"
+    return render_template('edit_user.html', module=module, data=std_list)
 
 
 @app.errorhandler(404)
